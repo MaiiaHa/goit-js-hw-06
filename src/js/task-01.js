9c83cb6e165b44557446e97b7ab151c47c5601aa
MaiiaHa <maiia.robeiko@ukr.net>
2023-02-16 00:20:15 +0100

    task-2, task-3

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -15,15 +15,15 @@ Elements: 3
 Category: Technologies
 Elements: 5 */
 
-const countEl = document.querySelectorAll('.item');
+const categoriesEl = document.querySelectorAll('.item');
 // console.log(countEl);
-console.log('Number of categories: ', countEl.length); // Number of categories: 3
+console.log('Number of categories: ', categoriesEl.length); // Number of categories: 3
 
-const allEl = document.querySelectorAll('h2');
+const categoryEl = document.querySelectorAll('h2');
 
-console.log('Category:', allEl[0].textContent); // Category: Animals
-console.log('Elements:', allEl[0].nextElementSibling.children.length); // Elements: 4
-console.log('Category:', allEl[1].textContent); // Category: Products
-console.log('Elements:', allEl[1].nextElementSibling.children.length); // Elements: 3
-console.log('Category:', allEl[2].textContent); // Category: Technologies
-console.log('Elements:', allEl[2].nextElementSibling.children.length); // Elements: 5
+console.log('Category:', categoryEl[0].textContent); // Category: Animals
+console.log('Elements:', categoryEl[0].nextElementSibling.children.length); // Elements: 4
+console.log('Category:', categoryEl[1].textContent); // Category: Products
+console.log('Elements:', categoryEl[1].nextElementSibling.children.length); // Elements: 3
+console.log('Category:', categoryEl[2].textContent); // Category: Technologies
+console.log('Elements:', categoryEl[2].nextElementSibling.children.length); // Elements: 5
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,3 +1,18 @@
+/*Напиши скрипт для створення галереї зображень 
+на підставі масиву даних. HTML містить список ul.gallery.
+
+<ul class="gallery"></ul>
+
+Використовуй масив об'єктів images для створення 
+елементів <img>, вкладених в <li>. Для створення розмітки 
+використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+Усі елементи галереї повинні додаватися в DOM за одну 
+операцію додавання.
+Додай мінімальне оформлення галереї флексбоксами або 
+грідами через CSS класи.
+*/
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +27,28 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryEl = document.querySelector('.gallery');
+// console.log(galleryEl);
+
+const imgGalleryEl = images.reduce((acc, image) => {
+  const listEl = document.createElement('li');
+  listEl.style.display = 'flex';
+  listEl.style.marginBottom = '10px';
+
+  const imgEl = document.createElement('img');
+  imgEl.src = image.url;
+  imgEl.alt = image.alt;
+  imgEl.width = 400;
+
+  acc.append(imgEl);
+
+  listEl.append(imgEl);
+  // console.log(listEl);
+
+  galleryEl.appendChild(listEl);
+
+  return acc;
+}, galleryEl);
+
+console.log(imgGalleryEl);
