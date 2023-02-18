9e9f60e78b9cb65eecbfd41dd63c86cdae877471
MaiiaHa <maiia.robeiko@ukr.net>
2023-02-18 17:43:33 +0100

    task6

--- js/task-06.js
+++ js/task-06.js
@@ -26,69 +26,17 @@
 */
 const textInputRef = document.querySelector('input#validation-input');
 const textLengthRef = document.querySelector('input[data-length="6"]');
-// console.log(textInputRef.classList.value);
+
 textInputRef.addEventListener('blur', checkEnteredData);
-// textInputRef.addEventListener('input', addTextInput);
-console.log(textInputRef.classList.value);
 
 function checkEnteredData(event) {
-  console.log('блюр собитие');
   const enteredDataLendth = event.currentTarget.value.length;
-  console.log(enteredDataLendth);
   const targetDataLength = Number(textLengthRef.dataset.length);
-  console.log(targetDataLength);
 
-  // textContentRef.textContent = event.currentTarget.value;
+  textInputRef.classList.remove('invalid', 'valid');
 
-  if (enteredDataLendth === targetDataLength && textInputRef.classList.value === '') {
-    console.log(textInputRef.classList.contains(''));
+  if (enteredDataLendth === targetDataLength) {
     return textInputRef.classList.add('valid');
-  } else if (enteredDataLendth === targetDataLength && textInputRef.classList.contains('invalid')) {
-    return textInputRef.classList.replace('invalid', 'valid');
-  } else if (enteredDataLendth !== targetDataLength && textInputRef.classList.contains('valid')) {
-    return textInputRef.classList.replace('valid', 'invalid');
-  } else if (textInputRef.classList.value === '') {
-    return textInputRef.classList.remove();
   }
-
   return textInputRef.classList.add('invalid');
-  /*
-1. якщо рівні символи 
-додаємо валід
-перевірка якщо валід, то валід
-перевірка якщо інвалід, то валід
-
-2. якщо не рівні символи
-додаємо інвалід
-перевірка якщо валід, то інвалід
-перевірка якщо інвалід, то інвалід і залишаємо
-
-  if (enteredDataLendth === targetDataLength && textInputRef.classList.contains('')) {
-    return textInputRef.classList.add('valid');
-  } else if (enteredDataLendth === targetDataLength && textInputRef.classList.contains('invalid')) {
-    return textInputRef.classList.replace('valid');
-  } else if (enteredDataLendth !== targetDataLength && textInputRef.classList.contains('valid')) {
-    return textInputRef.classList.replace('invalid');
-  }
-  return textInputRef.classList.replace('invalid');
-
-elem.classList.contains(cls) - повертає true або false, залежно від наявності класу cls в елемента.
-elem.classList.add(cls) - додає клас cls до списку класів елемента.
-elem.classList.remove(cls) - видаляє клас cls зі списку класів елемента.
-elem.classList.toggle(cls) - якщо відсутній клас cls, то додає його, якщо - присутній, навпаки - видаляє.
-elem.classList.replace(oldClass, newClass) - замінює існуючий клас oldClass на вказаний newClass.
-*/
-
-  // if (enteredDataLendth === targetDataLength && textInputRef.classList.value === '') {
-  //   return textInputRef.classList.add('valid');
-  // } else if (enteredDataLendth !== targetDataLength && textInputRef.classList.value === '') {
-  //   return textInputRef.classList.add('invalid');
-  // } else if (enteredDataLendth === targetDataLength && textInputRef.classList.value === 'valid') {
-  //   return textInputRef.classList.replace('invalid');
-  // } else if (enteredDataLendth === targetDataLength && textInputRef.classList.value === 'invalid') {
-  //   return textInputRef.classList.replace('valid');
-  // }
-  // return textInputRef.classList.add('invalid');
 }
-
-// повторно не перевіряє, додає два класи і все
